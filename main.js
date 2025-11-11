18ed7d3641c5eb85badd4aefa82ba803cf8a9da2
irham.sidi <irham.sidi@bpddiy.co.id>
2025-11-11 14:11:09 +0700

    Change code to Client Only

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("path");
-const express = require("express");
-const bodyParser = require("body-parser");
-
-let win;
-let alarmAcked = false;
 
 function createWindow() {
   win = new BrowserWindow({
-    width: 400,
-    height: 200,
+    width: 800,
+    height: 400,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
@@ -19,46 +14,5 @@ function createWindow() {
   win.loadFile("index.html");
   console.log("Desktop Alarm App Loaded!");
 }
-
-//Start Alarm
-function triggerAlarm() {
-  if (!alarmAcked) {
-    console.log("Triggering alarm...");
-    win.webContents.send("play-alarm");
-  }
-}
-
-//Stop Alarm
-function stopAlarm() {
-  console.log("Stopping alarm...");
-  win.webContents.send("stop-alarm");
-}
-
-//Webhook Server
-const webhookApp = express();
-webhookApp.use(bodyParser.json());
-
-webhookApp.post("/alert", (req, res) => {
-  const { status, message } = req.body;
-  console.log("Webhook received: ", req.body);
-
-  if (status === "firing") {
-    alarmAcked = false;
-    triggerAlarm();
-  } else if (status === "resolved") {
-    stopAlarm();
-  }
-
-  res.send("OK");
-});
-
-webhookApp.listen(5001, () => console.log("Webhook running on port 5001"));
-
-//Ack Button
-ipcMain.on("ack-alarm", () => {
-  console.log("Ack Button Clicked, Alarm Stopped!");
-  alarmAcked = true;
-  stopAlarm();
-});
 
 app.whenReady().then(createWindow);
--- renderer.js
+++ renderer.js
@@ -1,19 +1,59 @@
-const { ipcRenderer } = require("electron");
+const WebSocket = require("ws");
+
+const SERVER_ADDRESS = "ws://localhost:5002";
 
 const audio = new Audio("alarm.mp3");
 audio.loop = true;
+let ws;
 
-ipcRenderer.on("play-alarm", () => {
-  console.log("Playing alarm");
-  audio.play();
-});
+function connect() {
+  try {
+    ws = new WebSocket(SERVER_ADDRESS);
 
-ipcRenderer.on("stop-alarm", () => {
-  console.log("Stopping alarm");
+    ws.onopen = () => {
+      console.log("Connected to Server!");
+    };
+
+    ws.onmessage = (event) => {
+      const message = event.data;
+      console.log("Message received: ${message}");
+
+      if (message === "play-alarm") {
+        console.log("Playing Alarm...");
+        audio.play();
+      } else if (message === "stop-alarm") {
+        console.log("Stopping Alarm...");
+        audio.pause();
+        audio.currentTime = 0;
+      }
+    };
+
+    ws.onclose = () => {
+      console.log("Connection closed. Trying to reconnect again after 3s...");
+      setTimeout(connect, 3000);
+    };
+
+    ws.onerror = () => {
+      console.error("WebSocket error: ", err.message);
+      ws.close();
+    };
+  } catch (err) {
+    console.error("Unable to reach WebSocket Server: ", err);
+    setTimeout(connect, 3000);
+  }
+}
+
+//ACK Button
+document.getElementById("ackButton").addEventListener("click", () => {
+  console.log("Acknowledge by user, sending 'ack-alarm' to server.");
+  if (ws && ws.readyState === WebSocket.OPEN) {
+    ws.send("ack-alarm");
+  }
+
+  //Stop Audio on Current Client
   audio.pause();
   audio.currentTime = 0;
 });
 
-document.getElementById("ackButton").addEventListener("click", () => {
-  ipcRenderer.send("ack-alarm");
-});
+//Start Connection
+connect();
