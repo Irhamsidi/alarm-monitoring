82944c13aa120490b62527bdc3a8cdb6ab7b34ba
irham.sidi <irham.sidi@bpddiy.co.id>
2025-11-18 15:43:28 +0700

    Big Update UI/UX

--- renderer.js
+++ renderer.js
@@ -1,7 +1,15 @@
-const audio = new Audio("alarm.mp3");
+const audio = new Audio("alarm.wav");
 audio.loop = true;
 
 const ackButton = document.getElementById("ackButton");
+
+//Lottie Status Animation
+const lottieMainIdle = document.getElementById("lottie-main-idle");
+const lottieMainFiring = document.getElementById("lottie-main-firing");
+
+//Lottie Status Animation
+const lottieIdle = document.getElementById("lottie-idle");
+const lottieFiring = document.getElementById("lottie-firing");
 
 // Send ack
 ackButton.addEventListener("click", () => {
@@ -13,12 +21,26 @@ ackButton.addEventListener("click", () => {
 window.ipc.on("play-alarm", () => {
   console.log("Received 'play-alarm' from main process. Playing audio...");
   audio.play().catch((e) => console.error("Audio play failed: ", e));
+
+  //Main
+  lottieMainIdle.classList.add("hidden");
+  lottieMainFiring.classList.remove("hidden");
+  //Status
+  lottieIdle.classList.add("hidden");
+  lottieFiring.classList.remove("hidden");
 });
 
 window.ipc.on("stop-alarm", () => {
   console.log("Received 'stop-alarm' from main process. Stopping audio...");
   audio.pause();
   audio.currentTime = 0;
+
+  //Main
+  lottieMainFiring.classList.add("hidden");
+  lottieMainIdle.classList.remove("hidden");
+  //Status
+  lottieFiring.classList.add("hidden");
+  lottieIdle.classList.remove("hidden");
 });
 
 console.log("Renderer.js loaded and listeners are active.");
